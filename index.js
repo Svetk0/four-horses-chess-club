2f188c68b76b8f4b3b3cb15aa07aa47d590fffd9
svetk0 <sv.solomatnikova@gmail.com>
2024-06-22 12:43:25 +0300

    add adaptive in running line

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('hello');
+//import { ellipse } from './images/icons/running-line';
+
+const activateRunningBlock = () => {
+    const text =  'Дело помощи утопающим — дело рук самих утопающих! &middot Шахматы двигают вперед не только культуру, но и экономику! &middot Лед тронулся, господа присяжные заседатели!';
+    const runningBlock = document.querySelector('#running-text');
+    runningBlock.innerHTML = text;
+};
+
+activateRunningBlock();
