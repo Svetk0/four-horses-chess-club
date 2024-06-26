0956b2a3a38fc9920300ba7d9e6acf6955cd25b3
svetk0 <sv.solomatnikova@gmail.com>
2024-06-26 22:29:03 +0300

    mobile slider is working

--- index.js
+++ index.js
@@ -98,14 +98,81 @@ const insertPersonCards = (num) => {
         targetWrapper.appendChild(cards[i]);
         i++;
       }
-    // const card1 = createPersonCard(i);
-    // targetWrapper.appendChild(card1);
 }
 
+// CREATE SLIDER
+const createSlider = () => { 
+const slider = document.getElementById("slider");
+const arrowLeft = document.querySelector(".arrow-left");
+const arrowRight = document.querySelector(".arrow-right");
+const slides = document.querySelectorAll(".person");
+const bottom = document.getElementById("bottom");
 
+let currentSlideIndex = 0;
+const paginationCircles = [];
+const sliderWidth = slider.clientWidth;
+
+function createPaginationCircle() {
+    const div = document.createElement("div");
+    div.className = "pagination-circle";
+    bottom.appendChild(div);
+    paginationCircles.push(div);
+}
+
+function addPagination() {
+    slides.forEach(createPaginationCircle);
+    paginationCircles[0].classList.add("active");
+    paginationCircles.forEach((circle, index) => {
+        circle.addEventListener("click", () => changeSlide(index));
+    });
+}
+
+function addActiveClass() {
+    paginationCircles[currentSlideIndex].classList.add("active");
+    slides[currentSlideIndex].classList.add("active");
+}
+
+function removeActiveClass() {
+    paginationCircles[currentSlideIndex].classList.remove("active");
+    slides[currentSlideIndex].classList.remove("active");
+}
+
+function showSlide() {
+    //slider.style.transform = `translateX(-${currentSlideIndex * sliderWidth}px)`;
+    console.log('currentSlideIndex:',currentSlideIndex);
+}
+
+function changeSlide(slideIndex) {
+    removeActiveClass();
+    currentSlideIndex = slideIndex;
+    addActiveClass();
+    showSlide();
+}
+
+function nextSlide() {
+    let newSlideIndex = currentSlideIndex + 1;
+    if(newSlideIndex > slides.length - 1) {
+        newSlideIndex = 0;
+    }
+    changeSlide(newSlideIndex);
+}
+
+function previousSlide() {
+    let newSlideIndex = currentSlideIndex - 1;
+    if(newSlideIndex < 0) {
+        newSlideIndex = slides.length - 1;
+    }
+    changeSlide(newSlideIndex);
+}
+
+addPagination();
+arrowLeft.addEventListener("click", previousSlide);
+arrowRight.addEventListener("click", nextSlide);
+}
 // ------ START INITIALIZING ------
 activateRunningBlock();
 createCarouselButtons();
 insertPersonCards(dataPersons.length);
+createSlider();
 
 
