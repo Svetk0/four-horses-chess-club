f905cdb68c71f2ee888333845234c43faa229403
svetk0 <sv.solomatnikova@gmail.com>
2024-06-27 21:22:04 +0300

    add anchor links for title buttons

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // ---- RUNNING LINE -----
 const activateRunningBlock = () => {
-    const text =  'Дело помощи утопающим — дело рук самих утопающих! &middot Шахматы двигают вперед не только культуру, но и экономику! &middot Лед тронулся, господа присяжные заседатели!';
+    const text = 'Дело помощи утопающим — дело рук самих утопающих! &middot Шахматы двигают вперед не только культуру, но и экономику! &middot Лед тронулся, господа присяжные заседатели!';
     const runningBlock = document.querySelector('#running-text');
     runningBlock.innerHTML = text;
 };
 
 // ---- CAROUSEL BUTTONS ----
 // create svg carousel buttons
-const createCarouselButtons = () => { 
-    
+const createCarouselButtons = () => {
+
     const arrowLeft = 'M8.07664 15.923L1.15357 8.9999L8.07664 2.07682';
     const arrowRight = 'M1.92336 2.0769L8.84644 8.99998L1.92336 15.9231';
 
-    const createArrow = (arrowType, divClass) => { 
-        console.log('create svg '+divClass);
+    const createArrow = (arrowType, divClass) => {
+        console.log('create svg ' + divClass);
         const divBtn = document.querySelector(divClass);
         const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         const path = document.createElementNS(svg.namespaceURI, "path");
@@ -22,7 +22,7 @@ const createCarouselButtons = () => {
         divBtn.appendChild(svg);
     }
     createArrow(arrowRight, '.arrow-right');
-    createArrow(arrowLeft,'.arrow-left');
+    createArrow(arrowLeft, '.arrow-left');
 }
 
 // ---- CAROUSEL PARTICIPANTS ----
@@ -60,19 +60,19 @@ const dataPersons = [
     },
 ];
 
-const createDOMElem = (tagName, className) => { 
+const createDOMElem = (tagName, className) => {
     const newElement = document.createElement(tagName);
     newElement.classList.add(className);
     return newElement;
 }
 
-const createPersonCard = (i) => { 
+const createPersonCard = (i) => {
     const divPerson = createDOMElem('div', 'person');
     const imgPerson = createDOMElem('img', 'person__img');
     const namePerson = createDOMElem('div', 'person__name');
     const ratingPerson = createDOMElem('div', 'person__rating');
     const button = createDOMElem('button', 'button-info');
-    
+
     //values
     imgPerson.src = pathImg + dataPersons[i].img;
     namePerson.innerHTML = dataPersons[i].name;
@@ -88,86 +88,97 @@ const createPersonCard = (i) => {
     return divPerson;
 }
 
-const insertPersonCards = (num) => { 
+const insertPersonCards = (num) => {
     const targetWrapper = document.querySelector('.participants__content');
     let i = 0;
     let cards = [];
-    while (i < num) { 
+    while (i < num) {
         cards[i] = createPersonCard(i);
         cards.push(cards[i]);
         targetWrapper.appendChild(cards[i]);
         i++;
-      }
+    }
 }
 
-// CREATE SLIDER
-const createSlider = () => { 
-const slider = document.getElementById("slider");
-const arrowLeft = document.querySelector(".arrow-left");
-const arrowRight = document.querySelector(".arrow-right");
-const slides = document.querySelectorAll(".person");
-const bottom = document.getElementById("bottom");
+// CREATE SLIDER PARTICIPANTS
+const createSlider = () => {
+    const slider = document.getElementById("slider");
+    const arrowLeft = document.querySelector(".arrow-left");
+    const arrowRight = document.querySelector(".arrow-right");
+    const slides = document.querySelectorAll(".person");
+    const pagination = document.getElementById("pagination");
 
-let currentSlideIndex = 0;
-const paginationCircles = [];
-const sliderWidth = slider.clientWidth;
-
-function createPaginationCircle() {
-    const div = document.createElement("div");
-    div.className = "pagination-circle";
-    bottom.appendChild(div);
-    paginationCircles.push(div);
-}
-
-function addPagination() {
-    slides.forEach(createPaginationCircle);
-    paginationCircles[0].classList.add("active");
-    paginationCircles.forEach((circle, index) => {
-        circle.addEventListener("click", () => changeSlide(index));
-    });
-}
-
-function addActiveClass() {
-    paginationCircles[currentSlideIndex].classList.add("active-pagination");
+    let currentSlideIndex = 0;
+    const paginationCircles = [];
+    const sliderWidth = slider.clientWidth;
     slides[currentSlideIndex].classList.add("person--active");
-}
+    indicateCurrentSlide();
 
-function removeActiveClass() {
-    paginationCircles[currentSlideIndex].classList.remove("active-pagination");
-    slides[currentSlideIndex].classList.remove("person--active");
-}
+    function indicateCurrentSlide() {
+        const counterIndex = document.querySelector("#counter-index");
+        const counterLength = document.querySelector("#counter-length");
 
-function showSlide() {
-    //slider.style.transform = `translateX(-${currentSlideIndex * sliderWidth}px)`;
-    console.log('currentSlideIndex:',currentSlideIndex);
-}
-
-function changeSlide(slideIndex) {
-    removeActiveClass();
-    currentSlideIndex = slideIndex;
-    addActiveClass();
-    showSlide();
-}
-
-function nextSlide() {
-    let newSlideIndex = currentSlideIndex + 1;
-    if(newSlideIndex > slides.length - 1) {
-        newSlideIndex = 0;
+        counterIndex.textContent = currentSlideIndex + 1;
+        counterLength.textContent = '/'+slides.length;
     }
-    changeSlide(newSlideIndex);
-}
 
-function previousSlide() {
-    let newSlideIndex = currentSlideIndex - 1;
-    if(newSlideIndex < 0) {
-        newSlideIndex = slides.length - 1;
+    function createPaginationCircle() {
+        const div = document.createElement("div");
+        div.className = "pagination-circle";
+        pagination.appendChild(div);
+        paginationCircles.push(div);
     }
-    changeSlide(newSlideIndex);
-}
 
-addPagination();
-arrowLeft.addEventListener("click", previousSlide);
-arrowRight.addEventListener("click", nextSlide);
+    function addPagination() {
+        slides.forEach(createPaginationCircle);
+        paginationCircles[0].classList.add("active-pagination");
+        paginationCircles.forEach((circle, index) => {
+            circle.addEventListener("click", () => changeSlide(index));
+        });
+    }
+
+    function addActiveClass() {
+        paginationCircles[currentSlideIndex].classList.add("active-pagination");
+        slides[currentSlideIndex].classList.add("person--active");
+    }
+
+    function removeActiveClass() {
+        paginationCircles[currentSlideIndex].classList.remove("active-pagination");
+        slides[currentSlideIndex].classList.remove("person--active");
+    }
+
+    function showSlide() {
+        //slider.style.transform = `translateX(-${currentSlideIndex * sliderWidth}px)`;
+        console.log('currentSlideIndex:', currentSlideIndex);
+    }
+
+    function changeSlide(slideIndex) {
+        removeActiveClass();
+        currentSlideIndex = slideIndex;
+        addActiveClass();
+        showSlide();
+        indicateCurrentSlide();
+    }
+
+    function nextSlide() {
+        let newSlideIndex = currentSlideIndex + 1;
+        if (newSlideIndex > slides.length - 1) {
+            newSlideIndex = 0;
+        }
+        changeSlide(newSlideIndex);
+    }
+
+    function previousSlide() {
+        let newSlideIndex = currentSlideIndex - 1;
+        if (newSlideIndex < 0) {
+            newSlideIndex = slides.length - 1;
+        }
+        changeSlide(newSlideIndex);
+    }
+
+    addPagination();
+    arrowLeft.addEventListener("click", previousSlide);
+    arrowRight.addEventListener("click", nextSlide);
 }
 // ------ START INITIALIZING ------
 activateRunningBlock();
