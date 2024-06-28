97d343af4a751935578ef7d93b894ceec3430c79
svetk0 <sv.solomatnikova@gmail.com>
2024-06-29 00:21:48 +0300

    running line for all

--- scripts/running-line.js
+++ scripts/running-line.js
@@ -1,7 +1,10 @@
 // ---- RUNNING LINE -----
  const activateRunningBlock = () => {
     const text = 'Дело помощи утопающим — дело рук самих утопающих! &middot Шахматы двигают вперед не только культуру, но и экономику! &middot Лед тронулся, господа присяжные заседатели!';
-    const runningBlock = document.querySelector('#running-text');
-    runningBlock.innerHTML = text;
+    const runningBlock = document.querySelectorAll('.running-text');
+    console.log('runningBlock', runningBlock);
+    runningBlock.forEach((p) => { 
+       p.innerHTML = text;
+    })
  };
 export default activateRunningBlock;
