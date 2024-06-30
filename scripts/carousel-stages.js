a59f04643f5100a58a21110fe265da20d2417059
svetk0 <sv.solomatnikova@gmail.com>
2024-07-01 00:42:01 +0300

    find elem height

--- scripts/carousel-stages.js
+++ scripts/carousel-stages.js
@@ -1,7 +1,7 @@
-import { createDOMElem} from "./utils.js";
+import { createDOMElem } from "./utils.js";
 // ---- CAROUSEL STAGES ----
 
-const dataStages= [
+const dataStages = [
     {
         content: "Хозе-Строительство железнодорожной магистрали Москва-Васюки Капабланка",
         order: "1",
@@ -31,7 +31,20 @@ const dataStages= [
         order: "7",
     },
 ];
+export const determineMediaScreen = () => {
+    let numActiveSlides = 1;
+    let windowInnerWidth = document.documentElement.clientWidth;
 
+    console.log('windowInnerWidth', windowInnerWidth);
+    if (windowInnerWidth < 701) {
+        numActiveSlides = 2;
+    }
+    if (windowInnerWidth > 700) {
+        numActiveSlides = 1;
+    }
+
+    return numActiveSlides;
+}
 const determineStagesNumInBlock = () => {
     const elem = document.querySelector('.stage');
     let numActiveSlides = 1;
@@ -52,7 +65,7 @@ const determineStagesNumInBlock = () => {
 
 //Создаем карточку этапа
 const createStageCard = (i) => {
-    const divStage = createDOMElem('div', 'stage'); 
+    const divStage = createDOMElem('div', 'stage');
     const contentStage = createDOMElem('div', 'stage__content');
     const orderStage = createDOMElem('div', 'stage__order');
 
@@ -69,118 +82,139 @@ const createStageCard = (i) => {
 
 //Вставляем созданные карточки в DOM
 const insertStageCards = (num) => {
-    let numShowedSlides = 2;
+    //let numShowedSlides = determineMediaScreen();
+    let arrShowedSlides = [];
     const targetWrapper = document.querySelector('.stages__content');
     let i = 0;
     let cards = [];
     while (i < num) {
         cards[i] = createStageCard(i);
+        const divInner = createDOMElem('div', 'showed-stages-wrapper');
+        divInner.appendChild(cards[i]);
         cards.push(cards[i]);
         i++;
-    }
-    let attempt = num / numShowedSlides;
-    let k = 0;
-    let arrShowedSlides = [];
-
-    while (k < attempt) {
-        const divInner = createDOMElem('div', 'showed-stages-wrapper');
-        let j = 0;
-        while (j < numShowedSlides) {
-            divInner.appendChild(cards[j]);
-            j++;
-        }
-        arrShowedSlides.push(divInner);
-        targetWrapper.appendChild(arrShowedSlides[k]);
-        cards.splice(0, numShowedSlides);
-        k++;
+        //arrShowedSlides.push(divInner);
+        targetWrapper.appendChild(divInner);
     }
 }
 
-  // CREATE SLIDER STAGES
+const groupStages = () => { 
+    window.addEventListener("load", () => {
+        // Fully loaded!
+        const element = document.querySelector('.stage');
+    console.log('allCards', element.clientHeight);
+    //const element = parseFloat( getComputedStyle( element ).height ); 
+    const height = parseFloat( getComputedStyle( element ).height ); 
+    
+    console.log('height', height);
+    });
+    
+}
+// let attempt = num / numShowedSlides;
+// let k = 0;
+// let arrShowedSlides = [];
+
+// while (k < attempt) {
+//     const divInner = createDOMElem('div', 'showed-stages-wrapper');
+//     let j = 0;
+//     while (j < numShowedSlides) {
+//         cards[j].getAttribute('height');
+//         divInner.appendChild(cards[j]);
+//         j++;
+//     }
+//     arrShowedSlides.push(divInner);
+//     targetWrapper.appendChild(arrShowedSlides[k]);
+//     cards.splice(0, numShowedSlides);
+//     k++;
+// }
+
+
+// CREATE SLIDER STAGES
 export const createSliderStages = () => {
     insertStageCards(dataStages.length);
-        const arrowLeft = document.querySelector("#stages-btn-left");
-        const arrowRight = document.querySelector("#stages-btn-right");
-        const slides = document.querySelectorAll(".showed-stages-wrapper");
-        const pagination = document.getElementById("pagination-stages");
-        const counter = document.querySelector("#counter-stages");
-        const counterIndex = createDOMElem('div', 'counter__index');
-        const counterLength = createDOMElem('div', 'counter__length');
-        createCounter();
-        let currentSlideIndex = 0;
-       
-        const paginationCircles = [];
+    groupStages();
+    const arrowLeft = document.querySelector("#stages-btn-left");
+    const arrowRight = document.querySelector("#stages-btn-right");
+    const slides = document.querySelectorAll(".showed-stages-wrapper");
+    const pagination = document.getElementById("pagination-stages");
+    const counter = document.querySelector("#counter-stages");
+    const counterIndex = createDOMElem('div', 'counter__index');
+    const counterLength = createDOMElem('div', 'counter__length');
+    createCounter();
+    let currentSlideIndex = 0;
+
+    const paginationCircles = [];
     slides[currentSlideIndex].classList.add("showed-stages-wrapper--active");
     arrowLeft.setAttribute('disabled', '');
+    indicateCurrentSlide();
+
+    function createCounter() {
+        counter.appendChild(counterIndex);
+        counter.appendChild(counterLength);
+    }
+
+    function indicateCurrentSlide() {
+        counterIndex.textContent = (currentSlideIndex + 1);
+        counterLength.textContent = '/' + slides.length;
+    }
+
+    function createPaginationCircle() {
+        const div = createDOMElem('div', 'pagination__circle');
+        pagination.appendChild(div);
+        paginationCircles.push(div);
+    }
+
+    function addPagination() {
+        slides.forEach(createPaginationCircle);
+        paginationCircles[0].classList.add("stages__pagination--active");
+        paginationCircles.forEach((circle, index) => {
+            circle.addEventListener("click", () => changeSlide(index));
+        });
+    }
+
+    function addActiveClass() {
+        paginationCircles[currentSlideIndex].classList.add("stages__pagination--active");
+        slides[currentSlideIndex].classList.add("showed-stages-wrapper--active");
+    }
+
+    function removeActiveClass() {
+        paginationCircles[currentSlideIndex].classList.remove("stages__pagination--active");
+        slides[currentSlideIndex].classList.remove("showed-stages-wrapper--active");
+    }
+
+    function changeSlide(slideIndex) {
+        removeActiveClass();
+        currentSlideIndex = slideIndex;
+        addActiveClass();
         indicateCurrentSlide();
-
-        function createCounter() {
-            counter.appendChild(counterIndex);
-            counter.appendChild(counterLength);
-        }
-
-        function indicateCurrentSlide() {
-            counterIndex.textContent = (currentSlideIndex + 1);
-            counterLength.textContent = '/' + slides.length;
-        }
-
-        function createPaginationCircle() {
-            const div = createDOMElem('div', 'pagination__circle');
-            pagination.appendChild(div);
-            paginationCircles.push(div);
-        }
-
-        function addPagination() {
-            slides.forEach(createPaginationCircle);
-            paginationCircles[0].classList.add("stages__pagination--active");
-            paginationCircles.forEach((circle, index) => {
-                circle.addEventListener("click", () => changeSlide(index));
-            });
-        }
-
-        function addActiveClass() {
-            paginationCircles[currentSlideIndex].classList.add("stages__pagination--active");
-            slides[currentSlideIndex].classList.add("showed-stages-wrapper--active");
-        }
-
-        function removeActiveClass() {
-            paginationCircles[currentSlideIndex].classList.remove("stages__pagination--active");
-            slides[currentSlideIndex].classList.remove("showed-stages-wrapper--active");
-        }
-
-        function changeSlide(slideIndex) {
-            removeActiveClass();
-            currentSlideIndex = slideIndex;
-            addActiveClass();
-            indicateCurrentSlide();
-        }
+    }
 
     function nextSlide() {
         arrowLeft.removeAttribute('disabled');
-            let newSlideIndex = currentSlideIndex + 1;
-            if (newSlideIndex > slides.length - 2) {
-                arrowRight.setAttribute('disabled', '');
-            }
-            if (newSlideIndex > slides.length - 1) {
-                newSlideIndex = 0;
-            }
-            changeSlide(newSlideIndex);
+        let newSlideIndex = currentSlideIndex + 1;
+        if (newSlideIndex > slides.length - 2) {
+            arrowRight.setAttribute('disabled', '');
         }
+        if (newSlideIndex > slides.length - 1) {
+            newSlideIndex = 0;
+        }
+        changeSlide(newSlideIndex);
+    }
 
     function previousSlide() {
         arrowRight.removeAttribute('disabled');
-            let newSlideIndex = currentSlideIndex - 1;
-            if (newSlideIndex < 1) { 
-                arrowLeft.setAttribute('disabled', '');
-            }
-            if (newSlideIndex < 0) {
-                newSlideIndex = slides.length - 1;
-            }
-            changeSlide(newSlideIndex);
-       
+        let newSlideIndex = currentSlideIndex - 1;
+        if (newSlideIndex < 1) {
+            arrowLeft.setAttribute('disabled', '');
         }
+        if (newSlideIndex < 0) {
+            newSlideIndex = slides.length - 1;
+        }
+        changeSlide(newSlideIndex);
 
-        addPagination();
-        arrowLeft.addEventListener("click", previousSlide);
-        arrowRight.addEventListener("click", nextSlide);
     }
+
+    addPagination();
+    arrowLeft.addEventListener("click", previousSlide);
+    arrowRight.addEventListener("click", nextSlide);
+}
