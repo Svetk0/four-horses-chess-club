61e2a7198c755563468f95b43215483c94910486
svetk0 <sv.solomatnikova@gmail.com>
2024-07-02 21:46:57 +0300

    desktop correct

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import * as utils from "./scripts/utils.js";
 import joinText from "./scripts/join-text.js";
 import activateRunningLine from "./scripts/running-line.js";
 import createCarouselButtons from "./scripts/buttons-carousel.js";
-import { createSliderStages } from "./scripts/carousel-stages.js";
+//import { createSliderStages } from "./scripts/carousel-stages.js";
 import { createSliderParticipants } from "./scripts/carousel-participants.js";
+import { createSliderStages } from "./scripts/carousel-stages-actual.js";
 
 
 
@@ -12,7 +13,8 @@ import { createSliderParticipants } from "./scripts/carousel-participants.js";
 joinText();
 activateRunningLine();
 createCarouselButtons();
-createSliderStages();
 createSliderParticipants();
+createSliderStages();
+
 
 
--- scripts/carousel-stages-actual.js
+++ scripts/carousel-stages-actual.js
@@ -2,31 +2,37 @@ import { createDOMElem } from "./utils.js";
 // ---- CAROUSEL STAGES ----
 
 const dataStages = [
-    {
-        content: "Хозе-Строительство железнодорожной магистрали Москва-Васюки Капабланка",
-        order: "1",
-        combine: true,
-    },
-    {
-        content: "Открытие фешенебельной гостиницы «Проходная пешка» и других небоскрёбов",
-        order: "2",
-        combine: true,
-    },
+    [
+        {
+            content: "Хозе-Строительство железнодорожной магистрали Москва-Васюки Капабланка",
+            order: "1",
+            combine: true,
+            [Symbol.isConcatSpreadable]: true,
+        },
+        {
+            content: "Открытие фешенебельной гостиницы «Проходная пешка» и других небоскрёбов",
+            order: "2",
+            combine: true,
+        }
+    ],
+
     {
         content: "Поднятие сельского хозяйства в радиусе на тысячу километров: производство овощей, фруктов, икры, шоколадных конфет",
         order: "3",
         combine: false,
     },
-    {
-        content: "Строительство дворца для турнира",
-        order: "4",
-        combine: true,
-    },
-    {
-        content: "Размещение гаражей для гостевого автотранспорта",
-        order: "5",
-        combine: true,
-    },
+    [
+        {
+            content: "Строительство дворца для турнира",
+            order: "4",
+            combine: true,
+        },
+        {
+            content: "Размещение гаражей для гостевого автотранспорта",
+            order: "5",
+            combine: true,
+        }
+    ],
     {
         content: "Постройка сверхмощной радиостанции для передачи всему миру сенсационных результатов",
         order: "6",
@@ -38,124 +44,48 @@ const dataStages = [
         combine: false,
     },
 ];
-export const determineMediaScreen = () => {
-    let numActiveSlides = 1;
-    let windowInnerWidth = document.documentElement.clientWidth;
 
-    console.log('windowInnerWidth', windowInnerWidth);
-    if (windowInnerWidth < 701) {
-        numActiveSlides = 2;
-    }
-    if (windowInnerWidth > 700) {
-        numActiveSlides = 1;
-    }
-
-    return numActiveSlides;
-}
-const determineStagesNumInBlock = () => {
-    const elem = document.querySelector('.stage-card');
-    let numActiveSlides = 1;
-    let heightBlock = elem.getAttribute('height');
-
-    console.log('heightBlock', heightBlock);
-    if (heightBlock < 701) {
-        numActiveSlides = 1;
-    }
-    if (heightBlock > 700) {
-        numActiveSlides = 2;
-    }
-    if (heightBlock > 1200) {
-        numActiveSlides = 3;
-    }
-    return numActiveSlides;
-}
+let dataStagesDesktop = dataStages.reduce((card, current) =>
+    card.concat(current), []
+);
 
 //Создаем карточку этапа
-const createStageCard = (i) => {
+console.log('dataStages', dataStages, dataStagesDesktop);
+
+const createStageCardDesktop = (i) => {
+    const divInner = createDOMElem('div', 'showed-stages-wrapper');
     const divStage = createDOMElem('div', 'stage-card');
     const contentStage = createDOMElem('div', 'stage-card__content');
     const orderStage = createDOMElem('div', 'stage-card__order');
 
     //values
-    contentStage.innerHTML = dataStages[i].content;
-    orderStage.innerHTML = dataStages[i].order;
+    contentStage.innerHTML = dataStagesDesktop[i].content;
+    orderStage.innerHTML = dataStagesDesktop[i].order;
 
     //order
     divStage.appendChild(orderStage);
     divStage.appendChild(contentStage);
+    divInner.appendChild(divStage);
 
-    return divStage;
+    return divInner;
 }
 
 //Вставляем созданные карточки в DOM
-const insertStageCards = (num) => {
-    //let numShowedSlides = determineMediaScreen();
-    let arrShowedSlides = [];
-    const targetWrapper = document.querySelector('.stages__content');
+const insertStageCards = (data, func) => {
     let i = 0;
-    let cardsCombined = [];
-    let cardsSeparately = [];
-    let cards = [];
-    
-    while (i < num) {
-        cards[i] = createStageCard(i);
-        cards.push(cards[i]);
-        if (dataStages[i].combine) {
-            cardsCombined.push(cards[i]);
-            cardsSeparately.push('');
-        }
-        if (!dataStages[i].combine) {
-            cardsSeparately.push(cards[i]);
-            //cardsCombined.push('');
-        }
+    const targetWrapper = document.getElementById("slider-stages");
+    while (i < data.length) {
+        targetWrapper.appendChild(func(i));
         i++;
     }
-
-    const createCardWithWrapper = (array, numberOfShowedCards) => { 
-        let j = 0;
-        let arrWrapper = [];
-        while (j < array.length) { 
-            const divInner = createDOMElem('div', 'showed-stages-wrapper');
-            let k = 0;
-            while (k < numberOfShowedCards) { 
-                if (array[k] !== '') { 
-                    divInner.appendChild(array[k]);
-                    k++;
-                }
-            }
-            arrWrapper.push(divInner);
-            array.splice(0, numberOfShowedCards);
-            j++;
-        }
-        return arrWrapper;
-    }  
-    console.log('arrCards:', cardsCombined, cardsSeparately);
-    const cardsCombinedWrapped = createCardWithWrapper(cardsCombined, 2);
-    console.log('cardsCombinedWrapped:', cardsCombinedWrapped);
-    //const divInner = createDOMElem('div', 'showed-stages-wrapper');
-    //divInner.appendChild(cards[i]);
-    //targetWrapper.appendChild(divInner);
-}
-
-const groupStages = () => { 
-    window.addEventListener("load", () => {
-        // Fully loaded!
-        const element = document.querySelector('.stage-card');
-    console.log('allCards', element.clientHeight);
-    //const element = parseFloat( getComputedStyle( element ).height ); 
-    const height = parseFloat( getComputedStyle( element ).height ); 
-    
-    console.log('height', height);
-    });
-    
 }
 
 
 
 // CREATE SLIDER STAGES
 export const createSliderStages = () => {
-    insertStageCards(dataStages.length);
-    groupStages();
+    insertStageCards(dataStagesDesktop,createStageCardDesktop);
+
     const slider = document.getElementById("slider-stages");
     const arrowLeft = document.querySelector("#stages-btn-left");
     const arrowRight = document.querySelector("#stages-btn-right");
@@ -177,7 +107,7 @@ export const createSliderStages = () => {
     arrowLeft.addEventListener("click", previousSlide);
     arrowRight.addEventListener("click", nextSlide);
 
-     //------------------------- COUNTER --------------------
+    //------------------------- COUNTER --------------------
     function createCounter() {
         counter.appendChild(counterIndex);
         counter.appendChild(counterLength);
@@ -203,7 +133,7 @@ export const createSliderStages = () => {
         });
     }
 
- //------------------------- SLIDER --------------------
+    //------------------------- SLIDER --------------------
     function addActiveClass() {
         paginationCircles[currentSlideIndex].classList.add("stages__pagination--active");
         slides[currentSlideIndex].classList.add("showed-stages-wrapper--active");
@@ -216,7 +146,7 @@ export const createSliderStages = () => {
 
     function showSlide() {
         console.log('sliderWidth', sliderWidth);
-        slider.style.transform = `translateX(-${currentSlideIndex * (sliderWidth+20)}px)`;
+        slider.style.transform = `translateX(-${currentSlideIndex * (sliderWidth + 20)}px)`;
     }
 
     function changeSlide(slideIndex) {
